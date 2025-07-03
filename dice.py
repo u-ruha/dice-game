0756d5daf67d857d3d74ec264f77458311c24285
u-ruha <haruu.s3@icloud.com>
2025-07-03 16:38:22 +0900

    Create dice.py
    Dice game: initial revision

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+
+print("Rolling dice...")
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value: {die1 + die2}")
